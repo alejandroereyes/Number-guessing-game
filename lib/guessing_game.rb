ec593e30761bca77c4c5a20944f41fa7d0ba4280
Alejandro Reyes <alejandroereyes@gmail.com>
2015-05-20 13:48:00 -0500

    got prog running, opening greeting and user input method added

---
+++ lib/guessing_game.rb
@@ -0,0 +1,24 @@
+class NumberGuess
+
+  def start
+
+    opener
+
+    get_input
+
+  end # start method
+
+  def opener
+    system ('clear')
+    puts "Welcome to Number Guess 3000 \n"
+  end # opener method
+
+  def get_input
+    print "Guess a number between 1 - 100 : "
+    user_guess = gets.chomp.to_i
+  end # get_input method
+end # class
+
+game = NumberGuess.new
+
+game.start
