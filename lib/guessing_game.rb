10a3ea60916112790398dda9426f642dfb10c820
Alejandro Reyes <alejandroereyes@gmail.com>
2015-05-20 14:20:26 -0500

    created instance var for user guess

--- lib/guessing_game.rb
+++ lib/guessing_game.rb
@@ -2,13 +2,15 @@ class NumberGuess
 
   def initialize
     @correct_number = rand(1..100)
+    @user_guess = 0
   end
 
   def start
     # say hello to the user
-    opener
+    puts opener
     # prompt user for a guess
     get_input
+    # evaluate the user's guess
 
 
 
@@ -16,21 +18,25 @@ class NumberGuess
 
   def opener
     system ('clear')
-    puts "Welcome to Number Guess 3000 \n"
+    "Welcome to Number Guess 3000 "
   end # opener method
 
   def get_input
+    puts ""
     print "Guess a number between 1 - 100 : "
-    user_guess = gets.chomp
+    @user_guess = gets.chomp.to_i
     # validate user input
-    while user_guess.class != Fixnum || user_guess < 1 || user_guess > 100
+    while @user_guess < 1 || @user_guess > 100
       puts ""
       puts "!!  Invalid Entry !!"
       print "Guess a number between 1 - 100 : "
-      user_guess = gets.chomp
+      @user_guess = gets.chomp.to_i
     end # validation loop
-    user_guess.to_i!
+    @user_guess
   end # get_input method
+
+  def guess_checker
+  end # guess_checker method
 end # class
 
 game = NumberGuess.new
