cb63b7bda17afaa317569cb8fd2f9da182ac5f79
Alejandro Reyes <alejandroereyes@gmail.com>
2015-05-20 19:56:49 -0500

    adding test file, first test for opener passing

---
+++ spec/test_num_game.rb
@@ -0,0 +1,14 @@
+require 'test/unit'
+require_relative '../lib/guessing_game'
+
+class MyNumberGuessTest < Test::Unit::TestCase
+
+  def guess_setup
+    @game = NumberGuess.new
+  end
+
+  def test_opener_method
+    guess_setup # Given
+    assert_equal("Welcome to Number Guess 3000",@game.opener)# Then
+  end #
+end # class
